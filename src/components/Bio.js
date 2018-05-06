fb3ef3c4c913e6b24f40faa77e4e8a88180b356a
amiantos <bradroot@me.com>
2018-05-05 22:47:19 -0700

    remove eslint

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,26 +1,26 @@
 import React from 'react'
 
 class Bio extends React.Component {
-    render() {
-      return (
-        <div
-          style={{
-            borderBottom: '1px solid #eee'
-          }}
-        >
-          <p>
-            <strong>
-                Who? What?
-            </strong>{' '}
-            I'm just some dude. I build{' '}
-            <a href="https://www.numutracker.com">
-              Numu Tracker
-            </a>{' '}
-            and other things.
-          </p>
-        </div>
-      )
-    }
+  render () {
+    return (
+      <div
+        style={{
+          borderBottom: '1px solid #eee'
+        }}
+      >
+        <p>
+          <strong>
+              Who? What?
+          </strong>{' '}
+          I'm just some dude. I build{' '}
+          <a href="https://www.numutracker.com">
+            Numu Tracker
+          </a>{' '}
+          and other things.
+        </p>
+      </div>
+    )
   }
-  
-  export default Bio
+}
+
+export default Bio
