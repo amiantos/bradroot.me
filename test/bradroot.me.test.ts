ef8179ec0a27dd7399577672721c4a7e9efdb388
Brad Root <bradroot@me.com>
2024-10-26 11:46:19 -0700

    Initial commit

---
+++ test/bradroot.me.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BradrootMe from '../lib/bradroot.me-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/bradroot.me-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BradrootMe.BradrootMeStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
